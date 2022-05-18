6e156c412a47113f67fd2a6aa1d4a0def09d8a9e
Der Lord! <82890183+Booster1212@users.noreply.github.com>
2022-05-18 05:40:05 +0200

    Removing prod from cache

--- prod/configs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    githubRepositoryToSendIssue: '',
-    issueChannel: '976164749572194335',
-    commandPrefix: '!',
-};
-//# sourceMappingURL=index.js.map
